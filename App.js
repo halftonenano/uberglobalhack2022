34532e3c835f3b448dd85192bcce49e84c4bb021
Kazuki Oyama <76884973+flare30@users.noreply.github.com>
2022-07-23 18:32:54 +0900

    Co-authored-by: haruki0409 <haruki0409@users.noreply.github.com> Co-authored-by: halftone <halftonenano@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,20 +1,94 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+
+import {
+	StyleSheet
+} from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+import { Camera, CameraType } from 'expo-camera';
+
+import HelloWorld from './src/components/views/HelloWorld';
+import HomeScreen from './src/components/views/Home';
+import ScanScreen from './src/components/views/Scan';
+import LoginScreen from './src/components/views/Login';
+
+const MY_SECURE_AUTH_STATE_KEY = 'MySecureAuthStateKey';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
+	const [ hasPermission, setHasPermission ] = useState(null);
+	const [ type, setType ] = useState(CameraType.back);
+
+	useEffect(() => {
+		(async () => {
+			const { status } = await Camera.requestCameraPermissionsAsync();
+			setHasPermission(status === 'granted');
+		})();
+	});
+
 	return (
-		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
-			<StatusBar style="auto" />
-		</View>
+		<NavigationContainer styles={styles.navContainer}>
+			<Drawer.Navigator
+				initialRouteName='Scan'
+				screenOptions={{
+					drawerStyle: {
+					backgroundColor: '#b251db',
+					},
+					headerStyle: {
+						backgroundColor: '#b251db',
+						borderBottomColor: '#b251db',
+					},
+					headerShown: false,
+					// drawerHideStatusBarOnOpen: true,
+					swipeEdgeWidth: 300,
+					swipeMinDistance: 30,
+
+					drawerActiveBackgroundColor: '#c863f2',
+					drawerInactiveTintColor: 'white',
+					drawerActiveTintColor: 'white',
+
+					drawerType: 'slide',
+					overlayColor: 'transparent'
+				}}
+			>
+				<Drawer.Screen name='Home' component={HomeScreen}
+					options={{ drawerIcon: ({ focused, color }) => {
+						return (<Ionicons name={focused ? 'home' : 'home-outline'} size={24} color={color} />);
+					}}}
+				/>
+				<Drawer.Screen name='Scan' component={HelloWorld}
+					options={{ drawerIcon: ({ focused, color }) => {
+						return (<Ionicons name={focused ? 'camera' : 'camera-outline'} size={24} color={color} />);
+					}}}
+				/>
+				<Drawer.Screen name='Saved' component={HelloWorld}
+					options={{ drawerIcon: ({ focused, color }) => {
+						return (<Ionicons name={focused ? 'document-text' : 'document-text-outline'} size={24} color={color} />);
+					}}}
+				/>
+				<Drawer.Screen name='Login' component={LoginScreen}
+					options={{ drawerIcon: ({ focused, color }) => {
+						return (<Ionicons name={focused ? 'log-in' : 'log-in-outline'} size={24} color={color} />);
+					}}}
+				/>
+			</Drawer.Navigator>
+		</NavigationContainer>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
+		backgroundColor: '#b251db',
+		padding: 15,
+		paddingTop: 45,
+		// height: '100%'
 	},
+	navContainer: {
+		backgroundColor: '#b251db',
+	}
 });
