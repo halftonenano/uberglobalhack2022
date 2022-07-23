aff009296aca970e16c21e6a93167d8e70bce049
halftone <86031051+halftonenano@users.noreply.github.com>
2022-07-23 23:21:03 +0000

    updated saved screen styling + card layout

--- src/views/Saved.js
+++ src/views/Saved.js
@@ -12,12 +12,18 @@ export default function SavedScreen() {
 	useEffect(() => {
 		if (dataInit.current === undefined) {
 
+			let sampleText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Donec ultrices tincidunt arcu non sodales. Morbi tincidunt ornare massa eget. Pretium lectus quam id leo in vitae turpis massa. Faucibus a pellentesque sit amet porttitor. Cras fermentum odio eu feugiat pretium nibh. Feugiat in ante metus dictum at tempor commodo ullamcorper a. Bibendum at varius vel pharetra vel turpis. Aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices. Egestas sed sed risus pretium quam. Elit ut aliquam purus sit amet luctus venenatis lectus. Quis blandit turpis cursus in hac. Dui nunc mattis enim ut tellus.'
+
 			const exampleData = {
 				array: [
-					{ title: 'title1', time: '1658579054000', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing' },
-					{ title: 'title2', time: '1658579054000', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing' },
-					{ title: 'title3', time: '1658579054000', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing' },
-					{ title: 'title4', time: '1658579054000', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing' },
+					{ title: 'title1', time: '1658579054000', text: sampleText },
+					{ title: 'title2', time: '1658579054000', text: sampleText },
+					{ title: 'title3', time: '1658579054000', text: sampleText },
+					{ title: 'title4', time: '1658579054000', text: sampleText },
+					{ title: 'title1', time: '1658579054000', text: sampleText },
+					{ title: 'title2', time: '1658579054000', text: sampleText },
+					{ title: 'title3', time: '1658579054000', text: sampleText },
+					{ title: 'title4', time: '1658579054000', text: sampleText },
 				]
 			}
 
@@ -31,7 +37,7 @@ export default function SavedScreen() {
 
 	return (
 		<View style={styles.background}>
-			<Text style={styles.heading}> Saved </Text>
+			<Text style={styles.heading}>Saved</Text>
 			{/* <TouchableOpacity
 				style={styles.button}
 				onPress={() => storeData(JSON.stringify(exampleData))}
@@ -46,23 +52,25 @@ export default function SavedScreen() {
 			</TouchableOpacity> */}
 			{savedData !== undefined && (
 				<View>
-					<View style={styles.white}>
-						{savedData.array.map((item, index) => {
-							let timeStamp = dateFromMilsec(parseInt(item.time));
-							
-							return (
-								<View key={index} style={styles.card}>
-									<View style={styles.top}>
-										<Text style={styles.text}>{item.title}</Text>
-										<Text style={styles.text}>{timeStamp}</Text>
+					<SafeAreaView style={styles.white}>
+						<ScrollView>
+							{savedData.array.map((item, index) => {
+								let timeStamp = dateFromMilsec(parseInt(item.time));
+								
+								return (
+									<View key={index} style={styles.card}>
+										<View style={styles.top}>
+											<Text style={[ styles.text, styles.cardTitle ]}>{item.title}</Text>
+											<Text style={styles.text}>{timeStamp}</Text>
+										</View>
+										<View style={styles.passage}>
+											<Text numberOfLines={4} style={styles.size}>{item.text}</Text>
+										</View>
 									</View>
-									<View style={styles.passage}>
-										<Text>{item.text}</Text>
-									</View>
-								</View>
-							);
-						})}
-					</View>
+								);
+							})}
+						</ScrollView>
+					</SafeAreaView>
 				</View>
 			)}
 		</View>
@@ -71,9 +79,7 @@ export default function SavedScreen() {
 
 function dateFromMilsec(time) {
 	var dt = new Date(time);
-	var datee = dt.getFullYear() + "/" + (dt.getMonth() + 1) + "/" + dt.getDate();
-
-	console.log(datee);
+	var datee = dt.getFullYear() + " / " + (dt.getMonth() + 1) + " / " + dt.getDate();
 
 	return (datee);
 }
@@ -101,36 +107,43 @@ async function getData() {
 const styles = StyleSheet.create({
 	card: {
 		width:'90%',
-		height: '30%',
-		borderColor: '#b251db',
-		borderWidth: 3,
+		height: 155,
+		// borderColor: '#b251db',
+		// borderWidth: 2,
 		alignSelf: 'center',
 		marginVertical: 15,
 		borderRadius: 15,
 		backgroundColor: 'white',
 		shadowColor: 'black',
-		shadowOpacity: 0.2,
-		shadowRadius: 15,
+		shadowOpacity: 0.15,
+		shadowRadius: 12,
 	},
 	top: {
 		backgroundColor: '#ECD5F6',
-		borderTopLeftRadius: 12,
-		borderTopEndRadius: 12,
+		borderTopLeftRadius: 14,
+		borderTopEndRadius: 14,
 		flexDirection: 'row',
 		justifyContent: 'space-between',
 		padding: 10,
+		paddingHorizontal: 20,
+		// borderBottomColor: '#b251db',
+		// borderBottomWidth: 2,
+		zIndex: 3,
 	},
 	text: {
 		fontSize: 20,
+		color: '#b251db'
 	},
-	left: {
-
+	cardTitle: {
+		fontWeight: 'bold'
 	},
-	right: {
-
+	size: {
+		fontSize: 15,
+		color: '#7B7B7B'
+		// width: '100%'
 	},
 	passage: {
-		padding: 10,
+		padding: 20,
 	},
 	background: {
 		height: '100%',
@@ -153,6 +166,9 @@ const styles = StyleSheet.create({
 		height: '100%',
 		width: '100%',
 		zIndex: 2,
-		borderRadius: 15,
+		borderTopLeftRadius: 15,
+		borderTopRightRadius: 15,
+		borderTopColor: 'white',
+		borderTopWidth: 5,
 	}
 });
